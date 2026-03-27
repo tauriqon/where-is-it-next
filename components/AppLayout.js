c87eaa44d5509a65819dfe335b207125f02927c6
tauriqon <hansyo@me.com>
2026-03-27 11:05:30 +0900

    style: AppLayout.js 수정

--- components/AppLayout.js
+++ components/AppLayout.js
@@ -1,7 +1,7 @@
 export default function AppLayout({ children }) {
   return (
-    <main className="min-h-screen bg-white p-6">
-      <div className="mx-auto max-w-md">
+    <main className="min-h-screen bg-gray-50">
+      <div className="mx-auto flex min-h-screen w-full max-w-md flex-col px-5 pb-10 pt-6">
         {children}
       </div>
     </main>
